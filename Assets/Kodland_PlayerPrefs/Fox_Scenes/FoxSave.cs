780219c7d6acf77d6df0783d9bae55cc50bbe7cf
DawidWac13 <dwaclawik13@gmail.com>
2026-07-11 10:38:54 +0200

    Updae Wioska

---
+++ Assets/Kodland_PlayerPrefs/Fox_Scenes/FoxSave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class FoxSave : MonoBehaviour
+{
+    [SerializeField] TMP_Text saveWarning;
+    // Zapisywanie pozycji postaci gracza
+    public void SavePosition(Vector3 playerPos)
+    {
+        // Zapisywanie pozycji postaci gracza na wszystkich osiach w różnych miejscach 
+        PlayerPrefs.SetFloat("posX", playerPos.x);
+        PlayerPrefs.SetFloat("posY", playerPos.y);
+        PlayerPrefs.SetFloat("posZ", playerPos.z);
+        // Zapis danych
+        PlayerPrefs.Save();
+        saveWarning.text = "The save was successful!";
+        Invoke("DeleteText", 2f);
+    }
+
+    private void DeleteText()
+    {
+        saveWarning.text = "";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Jeśli spust portalu przekroczył obiekt z tagiem Gracza, wtedy
+        if (other.CompareTag("Player"))
+        {
+            // Pobieranie pozycji obiektu i przekazanie jej do metody SavePosition
+            Vector3 pos = other.transform.position;
+            SavePosition(pos);
+        }
+    }
+}
---
+++ Assets/Kodland_PlayerPrefs/Fox_Scripts/Fox_Logic.cs
@@ -0,0 +1,97 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Fox_Logic : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] TMP_Text coinsText;
+    Animator anim;
+    Rigidbody rb;    
+    Vector3 dir;
+    bool isGrounded;
+    int health = 100;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        if (PlayerPrefs.HasKey("posX"))
+        {
+            float loadedX = PlayerPrefs.GetFloat("posX");
+            float loadedY = PlayerPrefs.GetFloat("posY");
+            float loadedZ = PlayerPrefs.GetFloat("posZ");
+            transform.position = new Vector3(loadedX, loadedY, loadedZ);
+        }
+        if (PlayerPrefs.HasKey("Coins"))
+        {
+            GetCoin();
+        }
+    }
+
+    public void GetCoin()
+    {
+        coinsText.text = PlayerPrefs.GetInt("Coins").ToString();
+    }
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        dir = new Vector3(hor, 0.0f, ver);
+        dir = transform.TransformDirection(dir);
+        FoxAnim(hor, ver);
+        Jump();       
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + dir * moveSpeed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+        anim.SetBool("Jump", false);
+    }
+   
+    void FoxAnim(float h, float v)
+    {
+        if (dir.x != 0 || dir.z != 0)
+        {
+            anim.SetBool("Run", true);
+        }
+        else if (dir.x == 0 && dir.z == 0)
+        {
+            anim.SetBool("Run", false);
+        }
+
+        if (h <= -0.01f)
+        {
+            anim.SetFloat("Blend", 0f);
+        }
+        else if (h >= 0.01f)
+        {
+            anim.SetFloat("Blend", 1f);
+        }
+        else if (h < 0.1f && h > -0.1f && v != 0)
+        {
+            anim.SetFloat("Blend", 0.5f);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            isGrounded = false;
+            anim.SetBool("Jump", true);
+        }
+    }
+    public void ChangeHealth(int count)
+    {
+        // odejmowanie zdrowia
+        health -= count;
+        // jeśli zdrowie spada do zera lub niżej, to...
+        if (health <= 0)
+        {
+            this.enabled = false;
+        }
+    }
+}
